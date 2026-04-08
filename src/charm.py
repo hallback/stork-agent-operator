db26896f5b6048bfe6ba4f3d929df06d256df5d6
Johan Hallbäck <johan.hallback@krach.se>
2026-04-08 02:44:35 +0200

    A set of improvements for a more hassle free deploy:
    - added template and rendering of /etc/stork/agent.env
    - added charm-lib: operator_libs_linux.systemd
    - ensuring isc-stork-agent.service is enabled after install
    - added workload version display

--- src/charm.py
+++ src/charm.py
@@ -21,6 +21,7 @@ class StorkAgentOperatorCharm(ops.CharmBase):
         super().__init__(framework)
         framework.observe(self.on.install, self._on_install)
         framework.observe(self.on.start, self._on_start)
+        framework.observe(self.on.config_changed, self._on_config_changed)
         framework.observe(self.on["stork"].relation_changed, self._on_stork_relation_changed)
 
     def _on_install(self, event: ops.InstallEvent):
@@ -36,8 +37,47 @@ class StorkAgentOperatorCharm(ops.CharmBase):
             self.unit.set_workload_version(version)
         self.unit.status = ops.ActiveStatus()
 
+    def _on_config_changed(self, event: ops.ConfigChangedEvent):
+        logger.info(f"stork configuration changed: {event}")
+        relation_data = self._stork_relation_data
+        if relation_data is None:
+            return
+        agent_host, server_url = relation_data
+
+        stork_agent.render_and_reload(agent_host, server_url)
+
     def _on_stork_relation_changed(self, event: ops.RelationJoinedEvent):
         logger.info(f"stork relation changed: {event}")
+        relation_data = self._stork_relation_data
+        if relation_data is None:
+            return
+        agent_host, server_url = relation_data
+
+        stork_agent.render_and_reload(agent_host, server_url)
+
+    @property
+    def _stork_relation_data(self) -> tuple[str, str] | None:
+        # My IP for the stork relation
+        agent_host = str(self.model.get_binding("stork").network.bind_address)
+
+        # Get the remote ip on the stork relation
+        relation = self.model.get_relation("stork")
+        server_ip = ""
+        if relation is not None:
+            for unit in relation.units:
+                unit_data = relation.data[unit]
+                server_ip = unit_data.get("ingress-address") or unit_data.get("private-address") or ""
+                if server_ip:
+                    logger.info(f"_stork-relation-data(): Found server_ip {server_ip} on unit {unit}")
+                    break
+        if not server_ip:
+            logger.info(f"_stork-relation-data(): No server_ip found on the stork relation yet, skipping rendering")
+            return None
+
+        # TODO: We need to fix later so that this respects protocol and port
+        server_url = f"http://{server_ip}:8080"
+
+        return agent_host, server_url
 
 
 if __name__ == "__main__":  # pragma: nocover
--- src/stork_agent.py
+++ src/stork_agent.py
@@ -10,6 +10,15 @@ import logging
 import subprocess
 
 from charmlibs import apt
+from charms.operator_libs_linux.v1.systemd import (
+    service_enable,
+    service_running,
+    service_start,
+    service_restart,
+    service_failed
+)
+from jinja2 import Environment, FileSystemLoader
+
 logger = logging.getLogger(__name__)
 
 
@@ -18,29 +27,59 @@ logger = logging.getLogger(__name__)
 
 def install() -> None:
     """Install the workload (by installing a snap, for example)."""
-    # You'll need to implement this function.
+
+    # This command checks for dependencies, installs GPG keys and runs apt update
     cmd = (
         "curl -1sLf https://dl.cloudsmith.io/public/isc/stork/cfg/setup/bash.deb.sh"
         "| sudo bash"
     )
+
     try:
         subprocess.run(cmd, shell=True, check=True)
         apt.add_package(["isc-stork-agent"])
+        service_enable("isc-stork-agent")
     except Exception as e:
         # Throw an error to ensure automatic retry later
-        logger.error(f"Error installing stork-agent: {str(e)}")
+        logger.error(f"Error installing isc-stork-agent: {str(e)}")
         sys.exit(1)
+
 
 def start() -> None:
     """Start the workload (by running a commamd, for example)."""
-    # You'll need to implement this function.
-    # Ideally, this function should only return once the workload is ready to use.
+    if not service_running("isc-stork-agent"):
+        service_start("isc-stork-agent")
 
-
-# Functions for interacting with the workload, for example over HTTP:
+    if service_failed("isc-stork-agent"):
+        logger.error(f"Error starting isc-stork-agent")
+        sys.exit(1)
 
 
 def get_version() -> str | None:
     """Get the running version of the workload."""
-    # You'll need to implement this function (or remove it if not needed).
-    return None
+    # If we can't get the version, it is assumed the software isn't installed
+    try:
+        cmd = ["stork-agent", "--version"]
+        sp = subprocess.run(cmd, check=True, capture_output=True, encoding="utf-8")
+        logger.info(f"stork_agent.get_version()): got version: {sp.stdout}")
+        # Remove trailing newline
+        return sp.stdout.rstrip()
+    except Exception as e:
+        logger.warning(f"stork_agent.get_version()): Failed to get version: {e}")
+        return None
+
+
+def render_and_reload(agent_host, server_url) -> int:
+    # This should later only reload on actual config change
+    env = Environment(loader=FileSystemLoader("templates"),
+            keep_trailing_newline=True, trim_blocks=False)
+
+    agent_env_tmpl = env.get_template("agent.env.j2")
+    agent_env = agent_env_tmpl.render(
+        agent_host=agent_host,
+        server_url=server_url,
+    )
+    with open("/etc/stork/agent.env", "w") as file:
+        file.write(agent_env)
+
+    # reload/restart here
+    service_restart("isc-stork-agent")
